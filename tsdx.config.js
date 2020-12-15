82932a5ec9dc875c24b0b98d30c8b05bed67958d
Garrett Stevens <stevens.garrett.j@gmail.com>
2020-12-15 10:45:06 -0700

    Use TSDX for building package

---
+++ tsdx.config.js
@@ -0,0 +1,31 @@
+const globals = require('@jbrowse/core/ReExports/list').default
+
+globals.push('@jbrowse/core/util/rxjs', '@material-ui/core/SvgIcon')
+
+module.exports = {
+  rollup(config, options) {
+    if (options.format === 'umd') {
+      // If it's an external package in node_modules and is not in JBrowse's
+      // list of re-exported packages, bundle it in the UMD build.
+      const originalExternal = config.external
+      config.external = (...args) => {
+        const isExternal = originalExternal(...args)
+        if (isExternal) {
+          const id = args[0]
+          if (!globals.includes(id)) {
+            try {
+              require.resolve(id)
+              return false
+            } catch {}
+          }
+        }
+        return isExternal
+      }
+      // Let rollup know the global name of each JBrowse re-exported package
+      globals.forEach(global => {
+        config.output.globals[global] = `JBrowseExports.${global}`
+      })
+    }
+    return config
+  },
+}
