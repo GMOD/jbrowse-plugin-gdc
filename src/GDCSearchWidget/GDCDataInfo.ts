2439c9787c0e0623784442442e6caf4c338dd5b0
Caroline Bridge <83305007+carolinebridge-oicr@users.noreply.github.com>
2021-06-30 17:22:03 -0400

    auth ui adjustments and cleanup (#39)
    * adding better messaging to gdc auth ui and cleaning up some unused code
    
    * lint for gdcdatainfo
    
    * remove auth error message when new token has been entered
    
    * adding support for file urls
    
    * limit error message for readability

--- src/GDCSearchWidget/GDCDataInfo.ts
+++ src/GDCSearchWidget/GDCDataInfo.ts
@@ -16,37 +16,50 @@ const mapToAdapter: Map<string, Object> = new Map([
       prefix: 'bam',
     },
   ],
-]);
+  [
+    'Simple Nucleotide Variation',
+    {
+      config: { type: 'VariantTrack', adapter: { type: 'VcfAdapter' } },
+      prefix: 'vcf',
+    },
+  ],
+])
 
 /**
  * maps the data info to an appropriate data adapter
  * @param category the data category of the file
  * @param uri the uri of the data
- * @param isCompressed whether the data being retrieved is .gz and needs to be decompressed in its adapter
  * @param indexFileId the fileId of the index file that the data may require (BAM)
  * @returns an object containing the config type and the adapter object
  */
 export function mapDataInfo(
   category: string,
   uri: string,
-  isCompressed: boolean,
-  indexFileId?: string
+  indexFileId?: string,
 ) {
-  const configObject = mapToAdapter.get(category);
+  const configObject = mapToAdapter.get(category)
   let token = window.sessionStorage.getItem('GDCToken')
+
+  if (!token) token = ''
 
   if (configObject) {
     //@ts-ignore
-    configObject.config.adapter[`${configObject.prefix}Location`] = { uri: uri, authHeader: 'X-Auth-Token', authToken: `${token}` };
+    configObject.config.adapter[`${configObject.prefix}Location`] = {
+      uri: uri,
+      authHeader: 'X-Auth-Token',
+      authToken: `${token}`,
+    }
     if (indexFileId) {
       //@ts-ignore
-      configObject.config.adapter["index"] = {
-        location: { uri: `http://localhost:8010/proxy/data/${indexFileId}`, authHeader: 'X-Auth-Token', authToken: `${token}`},
-      };
+      configObject.config.adapter['index'] = {
+        location: {
+          uri: `http://localhost:8010/proxy/data/${indexFileId}`,
+          authHeader: 'X-Auth-Token',
+          authToken: `${token}`,
+        },
+      }
     }
   }
 
-  //configObject.adapter.isCompressed = isCompressed
-
-  return configObject;
+  return configObject
 }
