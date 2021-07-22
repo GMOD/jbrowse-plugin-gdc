721325224c016f0d265e1364f511fc5f19dd38dd
Caroline Bridge <cbridge@oicr.on.ca>
2021-07-22 11:50:24 -0400

    adding mafFeature, continue w implementation of adapter

--- src/MAFAdapter/MafAdapter.ts
+++ src/MAFAdapter/MafAdapter.ts
@@ -5,7 +5,8 @@ import {
 import { FileLocation, Region } from '@jbrowse/core/util/types'
 import { openLocation } from '@jbrowse/core/util/io'
 import { ObservableCreate } from '@jbrowse/core/util/rxjs'
-import SimpleFeature, { Feature } from '@jbrowse/core/util/simpleFeature'
+import MafFeature from './MafFeature'
+import { Feature } from '@jbrowse/core/util/simpleFeature'
 import { readConfObject } from '@jbrowse/core/configuration'
 import { AnyConfigurationModel } from '@jbrowse/core/configuration/configurationSchema'
 
@@ -21,29 +22,60 @@ export default class MafAdapter extends BaseFeatureDataAdapter {
     this.config = config
   }
 
-  private async getLines() {
+  private async readMaf() {
     const mafLocation = readConfObject(
       this.config,
       'mafLocation',
     ) as FileLocation
-    const lines = (await openLocation(mafLocation).readFile('utf8')) as string
-    return lines
-      .split('\n')
-      .slice(1)
-      .filter(f => !!f)
-      .map(line => {
-        const [id, chr, start, end, nprobes, mean] = line.split('\t')
-        return new SimpleFeature({
-          uniqueId: id,
-          id,
-          start: +start,
-          end: +end,
-          refName: chr,
-          nprobes: +nprobes,
-          mean: +mean,
-          score: +mean,
-        })
+    const fileContents = (await openLocation(mafLocation).readFile(
+      'utf8',
+    )) as string
+
+    const lines = fileContents.split('\n')
+    const header: string[] = []
+    const refNames: string[] = []
+    const rows: string[] = []
+    let columns: string[] = []
+    let refNameColumnIndex = 0
+    lines.forEach(line => {
+      if (line.startsWith('#')) {
+        header.push(line)
+      } else if (line) {
+        if (!columns) {
+          columns = line.split('\t')
+          refNameColumnIndex = columns.findIndex('Chromosome'.toLowerCase)
+        } else {
+          rows.push(line)
+          refNames.push(line.split('\t')[refNameColumnIndex])
+        }
+      }
+    })
+
+    return {
+      header: header.join('\n'),
+      lines: rows,
+      columns,
+      refNames: Array.from(new Set(refNames)),
+    }
+  }
+
+  private parseLine(line: string, columns: string[]) {
+    let mutationObject: any
+    line.split('\t').forEach((property: string, i: number) => {
+      mutationObject[columns[i]] = property
+    })
+    return mutationObject
+  }
+
+  private async getLines() {
+    const { columns, lines } = await this.readMaf()
+
+    return lines.map((line, index) => {
+      return new MafFeature({
+        mutation: this.parseLine(line, columns),
+        id: `${this.id}-maf-${index}`,
       })
+    })
   }
 
   private async setup() {
@@ -53,26 +85,8 @@ export default class MafAdapter extends BaseFeatureDataAdapter {
     return this.setupP
   }
 
-  private async determineRefNamesFromFile() {
-    const mafLocation = readConfObject(
-      this.config,
-      'mafLocation',
-    ) as FileLocation
-    const lines = (await openLocation(mafLocation).readFile('utf8')) as string
-
-    let refNames = lines
-      .split('\n')
-      .slice(1)
-      .filter(f => !!f)
-      .map(line => {
-        return line.split('\t')[1]
-      })
-
-    return Array.from(new Set(refNames))
-  }
-
   public async getRefNames(_: BaseOptions = {}) {
-    const refNames = await this.determineRefNamesFromFile()
+    const { refNames } = await this.readMaf()
     return refNames
   }
 
---
+++ src/MAFAdapter/MafFeature.ts
@@ -0,0 +1,64 @@
+import { objectTypeSpreadProperty } from '@babel/types'
+import { Feature } from '@jbrowse/core/util/simpleFeature'
+
+interface FeatureData {
+  [key: string]: unknown
+  refName: string
+  start: number
+  end: number
+  ncbi_build: string
+  strand: string
+  variant_classification: string
+  variant_type: string
+  reference_allele: string
+  tumor_seq_allele1: string
+  tumor_seq_allele2: string
+}
+
+export default class MafFeature implements Feature {
+  private mutation: any
+  private data: FeatureData
+  private _id: string
+
+  constructor(args: { mutation: any; id: string }) {
+    this.mutation = args.mutation
+    this.data = this.dataFromMutation(this.mutation)
+    this._id = args.id
+  }
+
+  get(field: string): any {
+    return this.data[field] || this.mutation[field]
+  }
+
+  set(): void {}
+
+  parent(): undefined {
+    return undefined
+  }
+
+  children(): undefined {
+    return undefined
+  }
+
+  tags(): string[] {
+    const t = [...Object.keys(this.data), ...Object.keys(this.mutation)]
+    return t
+  }
+
+  id(): string {
+    return this._id
+  }
+
+  dataFromMutation(mutation: any): FeatureData {
+    const featureData: FeatureData = {}
+    return featureData
+  }
+
+  toJSON(): any {
+    return {
+      uniqueId: this._id,
+      ...this.mutation,
+      ...this.data,
+    }
+  }
+}
